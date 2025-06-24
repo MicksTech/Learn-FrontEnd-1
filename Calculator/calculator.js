9a48b243e6f154a14ea73c7e1667a9378d2beb01
MicksTech <accpersonal371@gmail.com>
2025-06-24 15:46:16 +0800

    cal-commit

---
+++ Calculator/calculator.js
@@ -0,0 +1,22 @@
+const val = document.getElementById('inp');
+
+function AT_add(v) {
+    val.value += v;
+}
+
+function remove() {
+    val.value= val.value.substr(0, val.value.length - 1);
+}
+
+function cancel() {
+    val.value = '';
+}
+
+function exe() {
+    try {
+         val.value = eval(val.value)
+    }
+       catch (e) {
+        val.value = 'Error';
+    } 
+}
